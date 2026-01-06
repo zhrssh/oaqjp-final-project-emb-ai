3f42bae3bd9ec1bf518a5398f67f1c77669b1913
zhrssh <98146850+zhrssh@users.noreply.github.com>
2026-01-06 17:26:00 +0800

    Added server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+
+@app.route("/emotionDetector", methods=["GET"])
+def index():
+    if not "textToAnalyze" in request.args:
+        return jsonify(error_message="Missing 'textToAnalyze' query"), 400
+
+    # Call function
+    text_to_analyse = request.args["textToAnalyze"]
+    response = emotion_detector(text_to_analyse)
+
+    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
